661dffaa861631baf2d3e42b0bbe2d1abc6e882e
unknown <yashsaini.saini35@gmail.com>
2023-08-25 00:10:48 +0530

    Practice

---
+++ practice.sql
@@ -0,0 +1,27 @@
+show databases;
+use sakila;
+show tables;
+describe actor;
+desc actor;
+
+select * from actor;
+
+select actor_id from actor;
+
+select actor_id,first_name,last_name from actor;
+
+show tables;
+
+select * from city;
+
+select city_id,city_id*10 from city;
+
+select * from actor_info where actor_id=100;
+
+select actor_id,first_name from actor_info where actor_id>=5;
+
+select first_name, actor_id from actor_info where first_name="Johnny";
+
+
+
+
