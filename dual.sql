3d98f296815b44ec935503492b9a3913f156cc8e
unknown <yashsaini.saini35@gmail.com>
2023-09-05 17:33:45 +0530

    dual

--- dual.sql
+++ dual.sql
@@ -14,3 +14,21 @@ from dual;
 -- already available  with your system
 
 select now();
+
+--  stringfunction---
+select length ("tanu") from dual;
+select char_length("my dream world")from dual;
+select concat("d","5","g") from dual;
+select concat_ws("-","hello","bander","kaha") from dual;
+select ucase("hi");
+select substr(substr("hello",1,3),2); -- positive from left to right and negative from right to left------//nested substring
+select substr("hello",-1,3);-- for negative
+select substr("hello",1,3);-- for positive
+select instr("tanu","n"); -- to know the index of letter in a word-------
+select locate("a","rajasthan",3);-- first define letter then string then starting index
+select ltrim("         abc");-- ltrim removes the whitespace from string from left side
+select rtrim("abc        ");-- rtrim for right side
+select trim(leading" " from "       xyz        ");-- form start
+select trim(trailing" " from "       xyz        ");-- from ending
+select trim(both"z" from "zz zz         z");-- first 2z removed and then only last z is removed
+select lpad("hello",7,"#"); -- for left side addition to male character equ
